ec5b759124c8f9afe15e0f540711e17f2f4a58d4
Aron Aland <aron.aland@uptime.eu>
2025-05-23 21:24:27 +0300

    Init QuarterlyView webpart

---
+++ SPFx/QuarterlyViewWebpart/src/webparts/quarterlyView/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IQuarterlyViewWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'QuarterlyViewWebPartStrings' {
+  const strings: IQuarterlyViewWebPartStrings;
+  export = strings;
+}
